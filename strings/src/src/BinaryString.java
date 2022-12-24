ca7c31451a9d336b159c3140df89260d812fb412
Shuvankar Bhowmick <bhowmickshuvankar@gmail.com>
2022-12-24 12:54:20 +0530

    updated-commit

--- strings/src/src/BinaryString.java
+++ strings/src/src/BinaryString.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class BinaryString {
+public class BinaryString {
+    public static void main(String[] args) {
+        String str = "11011000110001";
+        System.out.println(binarySubstring(str.length(), str));
+    }
+    public static int binarySubstring(int a, String str)
+    {
+        // Your code here
+        int count = 0;
+        int ans = 0;
+
+        for (int i = 0; i < a; i++) {
+            if (str.charAt(i) == '1') {
+                count++;
+                if (count > 1)
+                    ans = ans + (count-1);
+            }
+        }
+
+        return ans;
+    }
 }
--- strings/src/src/FirstOccuranceOfSubstring.java
+++ strings/src/src/FirstOccuranceOfSubstring.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class FirstOccuranceOfSubstring {
+public class FirstOccuranceOfSubstring {
+    public static void main(String[] args) {
+        String s = "GeeksForGeeks";
+        String x = "eeks";
+        System.out.println(new FirstOccuranceOfSubstring().strstr(s, x));
+    }
+    int strstr(String s, String x)
+    {
+        // Your code here
+        int N = s.length();
+        int M = x.length();
+
+        //naive pattern searching algorithm
+        for (int i = 0; i <= N - M; i++) {
+            int j = 0;
+            for (; j < M; j++) {
+                if (s.charAt(i + j) != x.charAt(j))
+                    break;
+            }
+            if (j == M)
+                return i;
+        }
+        return -1;
+    }
 }
