414afbeee860e6543fa241009dc30b8930545799
Shuvankar Bhowmick <bhowmickshuvankar@gmail.com>
2023-01-11 13:53:06 +0530

    modified-commit

--- strings/src/src/CaseSort.java
+++ strings/src/src/CaseSort.java
@@ -20,43 +20,35 @@ public class CaseSort {
         char[] lower = new char[len];
         char[] upper = new char[len];
         char[] ans = new char[len];
-        int[] arr = new int[len];
-
+        Arrays.fill(lower, '}');
+        Arrays.fill(upper, '}');
         //1 for lowercase, 2 for uppercase
         int j = 0, k = 0;
         for (int i = 0; i < len; i++) {
             if (Character.isLowerCase(ch[i])) {
-                arr[i] = 1;
                 lower[j++] = ch[i];
             }
             else {
-                arr[i] = 2;
                 upper[k++] = ch[i];
             }
         }
         Arrays.sort(lower);
         Arrays.sort(upper);
         j = k = 0;
-        while (j < lower.length && !Character.isAlphabetic(lower[j])) j++;
-        while (k < upper.length && !Character.isAlphabetic(upper[k])) k++;
 
         //processing the resultant character array
         int i = 0;
         while (i < len) {
-            if (arr[i] == 1) {
-                // System.out.println(lower[j]);
+            if (Character.isLowerCase(ch[i])) {
                 ans[i] = lower[j];
                 j++;
             }
             else {
                 ans[i] = upper[k];
-                // System.out.println(upper[k]);
                 k++;
             }
             i++;
         }
-        // System.out.println();
-        // for (char chc : ans) System.out.print(chc);
         return new String(ans);
     }
 }
